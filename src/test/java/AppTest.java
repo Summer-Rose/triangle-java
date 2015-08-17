bb5fa16e4392833adfd31c66b13c4941b763fa6c
Summer Brochtrup & jocelyn alsdorf <student@epicodus.com>
2015-08-17 12:10:38 -0700

    Integration tests, unit tests, routing complete

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -15,4 +15,49 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Triangle Checker");
+  }
+
+  @Test
+  public void isNotATriangle() {
+    goTo("http://localhost:4567/");
+    fill("#sideOne").with("1");
+    fill("#sideTwo").with("2");
+    fill("#sideThree").with("10");
+    submit(".btn");
+    assertThat(pageSource()).contains("That's not a triangle, dummy!");
+  }
+
+  @Test
+  public void isEquilateral() {
+    goTo("http://localhost:4567/");
+    fill("#sideOne").with("2");
+    fill("#sideTwo").with("2");
+    fill("#sideThree").with("2");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your triangle is Equilateral!");
+  }
+
+  @Test
+  public void isIsosceles() {
+    goTo("http://localhost:4567/");
+    fill("#sideOne").with("2");
+    fill("#sideTwo").with("2");
+    fill("#sideThree").with("3");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your triangle is isosceles!");
+  }
+
+  @Test
+  public void isScalene() {
+    goTo("http://localhost:4567/");
+    fill("#sideOne").with("2");
+    fill("#sideTwo").with("3");
+    fill("#sideThree").with("4");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your triangle is scalene!");
+  }
 }
